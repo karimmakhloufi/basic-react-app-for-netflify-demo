10c2ccf606eda5e3814d23b9cdca8c4f60c73e1d
Karim Makhloufi <karim@kazoo.agency>
2020-11-18 17:58:50 +0100

    Get and display message from distant server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState("Default Message");
+
+  const getMessage = async () => {
+    const message = await axios.get("https://lc-nodejs.herokuapp.com/");
+    console.log(message);
+    setMessage(message.data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>I'm the app</p>
+      <p>{message}</p>
+      <button onClick={() => getMessage()}>Get New Message</button>
     </div>
   );
 }
